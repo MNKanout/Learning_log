bd99f0f710e3c47693b0a339d59b8cc8ef981ded
Syrwegian <mnkanout@gmail.com>
2020-03-31 18:03:50 +0200

    Added custom error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=9cmwykjq89zu-%f*dhyjkppf%6lr%ok9mds!h0lntbu=53e6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'learning_log/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
